ac715d0012ad57c1f03a44df14d66ba2e6f8072b
MartinaDR1 <derosemartina933@gmail.com>
2023-03-24 12:58:32 +0100

    jsnack5 commit

--- jsnack5/main.js
+++ jsnack5/main.js
@@ -1,3 +1,18 @@
 /*Scrivi una funzione che accetti tre argomenti:
 un array e due numeri ("a" più piccolo di "b" e "b" grande al massimo quanto il numero di elementi dell'array).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra "a" e "b"*/
+
+
+const arr =[1,2,3,4,5,6,7,8,9,10];
+
+myFunction(arr, 1, 10);
+
+
+function myFunction (arr, min, max) {
+  const newArray = arr.filter(i => {
+    if (i > min && i < max) {
+      return true;
+    }
+  });
+  console.log(newArray);
+}
